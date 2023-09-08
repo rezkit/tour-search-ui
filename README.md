fe812d70cef1ec0ec299f59cbc55dabfe371790f
Leo Adamek <leo.adamek@mrzen.com>
2023-09-08 16:03:10 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Tour Search UI
+==============
+
+A JavaScript library for building rich, real-time search UIs for
+small group tours powered by [RezKit](https://rezkit.app)
+
+
+Features
+--------
+
+* Small and lightweight
+* Sane, simple API
+* Web components
+
+
+Installation
+------------
+
+TBC: npm install & unpkg/jsdelivr
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+    plugins: [
+        dts()
+    ],
+
+    build: {
+        copyPublicDir: false,
+        lib: {
+            name: 'tour_search_ui',
+            entry: resolve(__dirname, 'src/main.ts'),
+            fileName: 'toursui',
+        }
+    }
+})
