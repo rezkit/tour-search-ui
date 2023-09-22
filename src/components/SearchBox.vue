1805d3a7d7a263eb7fe1699946b6edb2b742de33
Tom Hood <tom.hood@mrzen.com>
2023-09-22 10:53:15 +0100

    RK-533 Collapsible list mark-up and styles added.

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,3 +1,49 @@
+<script setup lang="ts">
+import { computed, inject, ref } from 'vue'
+import { Client, SEARCH_CLIENT } from '../main';
+import debounce from 'lodash-es/debounce'
+// Properties and events.
+//
+const emit = defineEmits(['update:model-value'])
+const props = defineProps({
+  modelValue: {
+    type: String
+  },
+
+  enableSuggestions: {
+    type: Boolean,
+    required: false,
+    default: true,
+  },
+
+  debounce: {
+    type: Number,
+    required: false,
+    default: 200
+  }
+})
+// Main variables.
+//
+const client = inject<Client>(SEARCH_CLIENT)
+const suggestions = ref()
+
+const value = computed({
+  get(): string {
+    return props.modelValue || ''
+  },
+
+  set(value: string) {
+    emit('update:model-value', value)
+  }
+})
+// Functions.
+//
+debounce(async () => {
+  suggestions.value = await client?.suggest({ ccy: 'GBP', q: value || '' })
+}, props.debounce)
+
+</script>
+
 <template>
     <div class="rkts-search-box">
         <input type="search" class="rk-input rk-input--search-box" v-model="value" />
@@ -7,45 +53,4 @@
     </div>
 </template>
 
-<script setup lang="ts">
-import { computed, inject, ref } from 'vue'
-import { Client, SEARCH_CLIENT } from '../main';
-import debounce from 'lodash-es/debounce'
-
-const props = defineProps({
-    modelValue: {
-        type: String
-    },
-
-    enableSuggestions: {
-        type: Boolean,
-        required: false,
-        default: true,
-    },
-
-    debounce: {
-        type: Number,
-        required: false,
-        default: 200
-    }
-})
-
-const emit = defineEmits(['update:model-value'])
-const client = inject<Client>(SEARCH_CLIENT)
-const suggestions = ref()
-
-const value = computed({
-    get(): string {
-        return props.modelValue || ''
-    },
-
-    set(value: string) {
-        emit('update:model-value', value)
-    }
-})
-
-debounce(async () => {
-    suggestions.value = await client?.suggest({ ccy: 'GBP', q: value || '' })
-}, props.debounce)
-
-</script>
+<style lang="scss"></style>
---
+++ src/components/filters/CollapsibleList.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { toRef } from 'vue'
+// Properties and events.
+//
+const props = defineProps<{
+  title: string
+}>()
+// Main variables.
+//
+const title = toRef(props, 'title')
+</script>
+
+<template>
+  <div class="rkts-collapsible-list">
+    <div class="rkts-collapsible-list__title">
+      <input class="rk-input rk-input--checkbox" type="checkbox" />
+      <p class="m-0">{{ title }}</p>
+    </div>
+    <div class="rkts-collapsible-list__content">
+      <ul class="rk-list"></ul>
+    </div>
+  </div>
+</template>
+
+<style lang="scss"></style>
