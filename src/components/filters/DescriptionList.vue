d288ab50b7df036607f7f67e6e1187e65352c1fc
Tom Hood <tom.hood@mrzen.com>
2023-09-22 11:10:07 +0100

    RK-533 Description list mark-up and styles added.

---
+++ src/components/filters/DescriptionList.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { toRef } from 'vue'
+// Properties and events.
+//
+const props = defineProps<{
+  title: string
+  text: string
+}>()
+// Main variables.
+//
+const title = toRef(props, 'title')
+const text = toRef(props, 'text')
+</script>
+
+<template>
+  <div class="rkts-description-list">
+    <div class="rkts-description-list__title">
+      <input class="rk-input rk-input--checkbox" type="checkbox" />
+      <p class="m-0">{{ title }}</p>
+    </div>
+    <div class="rkts-description-list__text">
+      {{ text }}
+      <hr class="rkts-description-list__line-break" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss"></style>
