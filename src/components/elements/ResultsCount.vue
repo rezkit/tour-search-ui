ac1b4e2086dc3523d69b990ed7538836b1b95106
Tom Hood <tom.hood@mrzen.com>
2023-11-16 12:08:54 +0000

    Result count style options added. Package version 0.2.20.

--- src/components/elements/ResultsCount.vue
+++ src/components/elements/ResultsCount.vue
@@ -6,16 +6,18 @@ const props = defineProps<{
   count: number
   label: string
   message: string
+  styleOpts?: string
 }>()
 // Main variables.
 //
 const label = toRef(props, 'label')
 const count = toRef(props, 'count')
 const message = toRef(props, 'message')
+const styleOpts = toRef(props, 'styleOpts')
 </script>
 
 <template>
-  <div class="rkts-results-count">
+  <div class="rkts-results-count" :class="[styleOpts]">
     <span class="rkts-results-count__label">{{ label }}</span>
     <span class="rkts-results-count__count">{{ count }}</span>
     <span class="rkts-results-count__message">{{ message }}</span>
