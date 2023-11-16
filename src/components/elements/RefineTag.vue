6847511546eae1453ebf735dc7cf80d8f8473823
Tom Hood <tom.hood@mrzen.com>
2023-11-16 09:33:37 +0000

    Add post message option to the refine tag.

--- src/components/elements/RefineTag.vue
+++ src/components/elements/RefineTag.vue
@@ -1,18 +1,26 @@
 <script setup lang="ts">
-import { toRef } from 'vue'
+import { ref, toRef } from 'vue'
 // Properties and events.
 //
 const emit = defineEmits(['process:clearFilter'])
 const props = defineProps<{
   filterValue: string
+  eventType?: string
 }>()
 // Main variables.
 //
 const filterValue = toRef(props, 'filterValue')
+const eventType = props.eventType ? toRef(props, 'eventType') : ref('emit')
 // Functions.
 //
 const processClick = function processClick() {
-  emit('process:clearFilter', filterValue.value)
+  if (eventType.value === 'window') {
+    window.postMessage({
+      rk_clear_filter: filterValue.value,
+    })
+  } else {
+    emit('process:clearFilter', filterValue.value)
+  }
 }
 </script>
 
