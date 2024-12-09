368955a05f0a3981b83d4cf6bec01aef75f7d90e
Tom Hood <tom.hood@mrzen.com>
2024-12-09 14:58:36 +0000

    Add apply button.

---
+++ src/components/elements/ApplyButton.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { toRef, ref } from 'vue'
+// Properties and events.
+//
+const emit = defineEmits(['process:applyFilters'])
+const props = defineProps<{
+  styleOpts?: string
+  text?: string
+}>()
+// Main variables.
+//
+const styleOpts = toRef(props, 'styleOpts')
+const text = props.text ? toRef(props, 'text') : ref('Apply')
+// Functions.
+//
+const processClick = function processClick() {
+  emit('process:applyFilters')
+}
+</script>
+<template>
+  <button
+    class="rk-btn"
+    :class="[styleOpts]"
+    type="button"
+    @click="processClick"
+  >
+    {{ text }}
+  </button>
+</template>
