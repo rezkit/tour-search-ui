7e90f156df9f548b19ab62288b28f46cdc5ae7a6
Tom Hood <tom.hood@mrzen.com>
2025-03-10 14:59:29 +0000

    Package release 0.2.53. Tag count support.

--- src/components/elements/RefineTag.vue
+++ src/components/elements/RefineTag.vue
@@ -1,18 +1,28 @@
 <script setup lang="ts">
-import { ref, toRef } from 'vue'
+import { computed, ref, toRef } from 'vue'
 // Properties and events.
 //
 const emit = defineEmits(['process:clearFilter'])
 const props = defineProps<{
+  filterCount?: number
   filterValue: string
   filterText: string
   eventType?: string
 }>()
 // Main variables.
 //
+const filterCount = toRef(props, 'filterCount')
 const filterValue = toRef(props, 'filterValue')
 const filterText = toRef(props, 'filterText')
 const eventType = props.eventType ? toRef(props, 'eventType') : ref('emit')
+// Computed.
+//
+const hasCount = computed(
+  () =>
+    filterCount.value !== undefined &&
+    filterCount.value !== null &&
+    filterCount.value >= 0
+)
 // Functions.
 //
 const processClick = function processClick() {
@@ -30,6 +40,7 @@ const processClick = function processClick() {
   <p class="rkts-refine-tag" @click="processClick">
     <i class="rkts-refine-tag__icon fontello icon-sys-close"></i>
     <span class="rkts-refine-tag__text">{{ filterText }}</span>
+    <b v-if="hasCount" class="rk-text rk-text--count"> ({{ filterCount }}) </b>
   </p>
 </template>
 
